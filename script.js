3aa064881000680a99c26f36a3ef100778dca502
deepeshyadavrd <raoshsbh6@gmail.com>
2024-07-25 15:12:17 +0530

    created task with date as id

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const formElement = document.querySelector(".form");
 
-formElement.addEventListener("submit");
+formElement.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    // console.log(e.target);
+
+    const inputtedValue = e.target.elements['input-add-todo'].value;
+    if(!inputtedValue){
+        return;
+    }
+    const todo = {
+        id: new Date().getTime().toString(),
+        todo: inputtedValue,
+    }
+
+    console.log(todo);
+});
