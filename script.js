f6061290c4f92d0af8457b323394792a6b3a086c
deepeshyadavrd <raoshsbh6@gmail.com>
2024-07-25 15:25:05 +0530

    task displayed in list on page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const formElement = document.querySelector(".form");
+const todoListElement = document.querySelector(".todo-list");
 
 formElement.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -14,4 +15,20 @@ formElement.addEventListener("submit", (e)=>{
     }
 
     console.log(todo);
+    createTodo(todo);
 });
+
+const createTodo = (todo) =>{
+    createTodoItem(todo);
+}
+
+const createTodoItem = ({todo, id}) => {
+    const todoItem = `<li id=${id} class="todo-item">
+                    <p>${todo}</p>
+                    <button class="todo-item-delete-btn">
+                        <i class="ri-close-line"></i>
+                    </button>
+                </li>`;
+
+    todoListElement.insertAdjacentHTML("beforeend",todoItem)
+}
